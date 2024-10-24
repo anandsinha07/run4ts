01d134925a5a824c7db3d35359e57dba776770d6
Anand Sinha <anandkumarsinha07@gmail.com>
2024-10-24 16:30:00 +0530

    catch errors

--- hellots/index.js
+++ hellots/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("Hello TS");
+console.log("Hello, TS");
+const students = [{
+        name: "Anand",
+        score: 70
+    },
+    {
+        name: "Sinha",
+        score: 20
+    }];
+for (const student of students) {
+    console.log("Name", student.name);
+}
--- hellots/index.ts
+++ hellots/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello TS");
+console.log("Hello, TS");
+
+const students = [{
+    name: "Anand",
+    score: 70
+},
+{
+    name: "Sinha",
+    score: 20
+}];
+
+for (const student of students) {
+    console.log("Name", student.name);
+}
